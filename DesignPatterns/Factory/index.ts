0d57d97835ca7464cb2d0c73e7961fbcb1b71488
Denuwan Liyanage <denuwan.sds@gmail.com>
2022-10-19 14:59:49 +0530

    Implement factory design pattern

---
+++ DesignPatterns/Factory/index.ts
@@ -0,0 +1,58 @@
+enum CakeType {
+    ChocolateCake,
+    VanillaCake,
+    FruitCake
+}
+
+class Order {
+    public constructor(
+        public orderNum: number,
+        public cakeType: CakeType,
+        public customer: string,
+    ) {
+    }
+
+    public toString():string{
+        return `Order number is ${this.orderNum}, cake type is ${this.cakeType}, customer is ${this.customer}`;
+    }
+}
+
+abstract class CakeBase {
+    public constructor(public order: Order, public weight: number) {
+    }
+
+    public toString(): string {
+        return `Cake made for order ${this.order}`;
+    }
+
+}
+
+class ChocolateCake extends CakeBase {
+}
+
+class VanillaCake extends CakeBase {
+}
+
+class FruitCake extends CakeBase {
+}
+
+class CakeFactory {
+    public constructor(public readonly gramsPerCake: number) {
+    }
+
+    public bakeCake(order: Order): CakeBase {
+        switch (order.cakeType) {
+            case CakeType.ChocolateCake:
+                return new ChocolateCake(order, this.gramsPerCake);
+            case CakeType.VanillaCake:
+                return new VanillaCake(order, this.gramsPerCake);
+            case CakeType.FruitCake:
+                return new FruitCake(order, this.gramsPerCake);
+        }
+    }
+}
+
+let factory = new CakeFactory(150);
+let order = new Order(1, CakeType.ChocolateCake, "Sunimal");
+let cake =factory.bakeCake(order);
+console.log(cake.toString());
